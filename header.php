73f01d8b030a936c0c51c5f046539f18400515d0
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-09-05 03:34:34 +0600

    --- Auto-Git Commit ---

--- header.php
+++ header.php
@@ -9,31 +9,63 @@
     src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
     integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
     crossorigin="anonymous"></script>
-    <script>
-    $(function() {
-    $(".toggle").on("click", function() {
-        if ($(".item").hasClass("active")) {
-            $(".item").removeClass("active");
-        } else {
-            $(".item").addClass("active");
-        }
-    });
-});
-    </script>
+   
 </head>
 <body>
-    <nav>
-        <ul class="menu">
-            <li class="logo"><img src="./images/logo_two.png" style="height:25px;background-color:azure;" alt="" ></li>
-            <li class="item"><a style="color:white" href="./index.php">Home</a></li>
-            <li class="item"><a style="color:white" href="./About_us.php">About</a></li>
-            <li class="item"><a style="color:white" href="./contact.php">Contact</a></li>
-            </li>
-            <li class="item button"><a href="./php/login.php">Log In</a></li>
-            <li class="item button secondary"><a href="./php/register.php">Sign Up</a></li>
-            <li class="toggle" ><span class="bars"></span></li>
-        </ul>
-    </nav>
+    
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="index.html">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item m-auto">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="catagories.php">Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="About_us.php">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Post.php">Our posts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cart.php">Cart </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+            </ul>
+
+            <form class="form-inline my-2 my-lg-0">
+                <div class="input-group input-group-sm">
+                    <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search...">
+                    <div class="input-group-append">
+                        <button type="button" class="btn btn-secondary btn-number">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+                <a class="btn btn-success btn-sm ml-3" href="cart.html">
+                    <i class="fa fa-shopping-cart"></i> Cart
+                    <span class="badge badge-light">3</span>
+                </a>
+            </form>
+        </div>
+    </div>
+</nav>
   
 </body>
 </html>
