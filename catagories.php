89fa1eee3e327c9530fc9a4ffe2ece900aa35d8e
MdsalahUddin313 <nabidbhuia314@gmail.com>
2020-09-06 13:38:42 +0600

    --- Auto-Git Commit ---

--- catagories.php
+++ catagories.php
@@ -72,7 +72,7 @@
 ?>
               <div class="col-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img class="card-img-top" src="<?php $Link['images'] ?>" alt="Card image cap">
+                        <img class="card-img-top" src='<?php $Link['images']?>' alt="Card image cap">
                         <div class="card-body">
                             <h4 class="card-title"><a href="product.html" title="View Product"><?php echo $link['title'] ?></a></h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
